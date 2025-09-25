effd80bc1f249662e09ea64db5f242c991b1dea7
bananaboiii-53 <127419046+bananaboiii-53@users.noreply.github.com>
2025-09-25 10:57:35 -0500

    Update post-create.sh

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -8,4 +8,7 @@ echo "Codespace setup complete. Running npm install..."
 # It automatically installs the project dependencies from package.json.
 npm install
 
-echo "Dependencies installed. You can now start the server with 'npm start'"
+echo "Dependencies installed. Starting the server now..."
+
+# Automatically start the server.
+npm start
